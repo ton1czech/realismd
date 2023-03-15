47e4f84bf4400326958b8694fd16faab8305a9a8
ton1czech <tonyasek007@gmail.com>
2023-03-15 16:30:06 +0100

    🎁NEW: process markdown into html elements

--- src/containers/Markdown.jsx
+++ src/containers/Markdown.jsx
@@ -1,7 +1,15 @@
+import { Converter } from 'showdown'
+
+import '../styles/markdown.css'
+
 export default function Markdown({ content }) {
+  const processor = new Converter()
+  const processed = processor.makeHtml(content)
+
   return (
-    <div>
-      <p>{content}</p>
-    </div>
+    <div
+      className='processed flex flex-col'
+      dangerouslySetInnerHTML={{ __html: processed }}
+    ></div>
   )
 }
