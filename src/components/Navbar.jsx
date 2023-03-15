7a47f92e97a9f4d1a75c79626b41038926c484c4
ton1czech <tonyasek007@gmail.com>
2023-03-15 17:22:07 +0100

    👌IMPROVE: nav styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 export default function Navbar() {
   return (
     <div className=' mb-2 bg-slate-300'>
-      <h1 className='mx-auto max-w-screen-2xl text-2xl font-semibold'>
-        REALISMD - real time markdown processor
+      <h1 className='mx-auto flex max-w-screen-2xl items-center gap-1 text-2xl font-semibold'>
+        REALISMD -
+        <span className='text-lg'> real time markdown processor </span>
       </h1>
     </div>
   )
